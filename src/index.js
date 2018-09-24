6b33facc90e3d61f3f9030a2c52c69a81127eea0
Valentin Lopan <valiantsin_lopan@live.com>
2018-09-25 00:50:05 +0300

    implement solveEquation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  equation = equation.replace(/\s/g, "");
+  let a = parseInt(equation.split('*x^2')[0]);
+  let b = parseInt(equation.split('*x^2')[1].split('*x')[0]);
+  let c = parseInt(equation.split('*x^2')[1].split('*x')[1]);
+  let discriminant = Math.sqrt((b * b) - 4 * a * c);
+  let denominator = 2 * a;
+  let root1 = round((-b + discriminant) / denominator);
+  let root2 = round((-b - discriminant) / denominator);
+  return [root1, root2].sort((a, b) => a - b);
 }
+
+function round(value) {
+  return Math.round(value * 100) / 100;
+};
